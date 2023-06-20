e2cd281ba0ab6403ee911dfaca68b564d3a061a2
Dog2077 <1002302241@qq.com>
2023-06-21 00:22:12 +0800

    Rewrite load info for api

--- apps/Analysis/views.py
+++ apps/Analysis/views.py
@@ -4,14 +4,22 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from Analysis.tools import *
+from Login.views import confirmUser
 
 
 class loadInfo(APIView):
     def post(self, request):
-        param = request.data["data"]
-        fileType = checkFileType(param)
-        fileData = base64.b64decode(FileData(param))
-        addFile(fileData, fileType)
-        return Response({
-            "type": fileType
-        })
+        hash_code = request.data["token"]
+        uid = confirmUser(hash_code)
+        if uid == -1:
+            return Response({
+                "error": "user not allowed"
+            })
+        else:
+            param = request.data["baseByte"]
+            fileType = checkFileType(param)
+            fileData = base64.b64decode(FileData(param))
+            addFile(fileData, fileType)
+            return Response({
+                "success": "add a worker file"
+            })
